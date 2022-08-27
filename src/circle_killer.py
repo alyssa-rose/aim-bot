b9455d774b2a57fcf7e674c7440e76c2bfe8c14a
Alyssa Bekai Rose <alyssa.s.rose5@gmail.com>
2022-08-27 16:30:33 -0400

    documentation

--- src/circle_killer.py
+++ src/circle_killer.py
@@ -4,34 +4,61 @@ Created on Sat Aug 27 15:40:58 2022
 
 @author: Alyssa
 """
-import serial
+
+# Standard library imports.
 import time
 
-import numpy as np
+# Related third party imports.
 import cv2
+import numpy as np
+import serial
 
 
 class CircleKiller():
     """ Class to locate, track, and shoot circles
     as seen in AimLabs. Original concept by the lovely 
-    Daniel Bekai"""
+    Daniel Bekai ;)
+    """
     
-    def __init__(self, arduino_port: str = 'COM7', baud_rate: int = 115200, camera_id: int = 0) -> None:
+    def __init__(self, arduino_port: str = 'COM7', 
+                 baud_rate: int = 115200, 
+                 camera_id: int = 0,
+                 screen_center_width: int = 640,
+                 screen_center_height: int = 360) -> None:
         '''
+        Initializes the class instance.
+        
         Parameters
         ----------
         arduino_port : str, optional
-            DESCRIPTION. The default is 'COM7'.
+            The Arduino port to connect to. This can be found in the settings
+            of the Arduino interface. The default is 'COM7'.
+            
         baud_rate : int, optional
-            DESCRIPTION. The default is 115200.
+            Rate at which information is sent in the communication
+            channel. The default is 115200.
+            
         camera_id : int, optional
-            DESCRIPTION. The default is 0.
+            ID of the camera to use. If you only have one camera, then
+            it will be 0. The default is 0.
+            
+        screen_center_width : int, optional
+            1/2 width of the screen that the camera is pointed at. Used to
+            find the center of the screen. The default is 640.
+            
+        screen_center_height : int, optional
+            1/2 height of the screen that the camera is pointed at. Used to
+            find the center of the screen. The default is 360.
 
         Returns
         -------
         None.
 
         '''
+        # Set the screen width and height
+        self.screen_width = screen_center_width
+        self.screen_height = screen_center_height
+        
         # initialize the connection to the arduino
         self.serialcomm = serial.Serial(arduino_port, baud_rate)
         self.serialcomm.timeout = 1
@@ -43,16 +70,22 @@ class CircleKiller():
         self.prevcircle = None
         
         
-    def control_law(self, dx: int, dy: int, dist_from_center: int) -> str:
+    def control_law(self, dx: int, dy: int, dist_from_center: float) -> str:
         '''
+        Performs the PID control for moving the mouse. These parameters may
+        change according to your needs!
+        
         Parameters
         ----------
         dx : int
             DESCRIPTION.
+            
         dy : int
             DESCRIPTION.
-        dist_from_center : int
-            DESCRIPTION.
+            
+        dist_from_center : float
+            The Euclidean distance of the circle from
+            the center of the screen.
 
         Returns
         -------
@@ -75,6 +108,8 @@ class CircleKiller():
     
     def dist(self, x1: float, y1: float, x2: float, y2: float) -> float: 
         '''
+        Calculates the Euclidean distance.
+        
         Parameters
         ----------
         x1 : float
@@ -96,12 +131,14 @@ class CircleKiller():
     
     def update(self) -> bool:
         '''
-        
+        Grabs a new frame, determines the closest circle to the
+        center of the screen, and moves the mouse.
 
         Returns
         -------
         bool
-            DESCRIPTION.
+            False if the user has pushed 'q' to exit the program, 
+            and True otherwise.
 
         '''
         ret, frame = self.cap.read()
@@ -122,7 +159,7 @@ class CircleKiller():
         circles = cv2.HoughCircles(result, cv2.HOUGH_GRADIENT, 1.2,
                                    100, param1=100, param2=30, minRadius=30, maxRadius=100)
 
-        cv2.circle(frame, (640, 360), 5, (0, 0, 255), 2)
+        cv2.circle(frame, (self.screen_width, self.screen_height), 5, (0, 0, 255), 2)
         if circles is not None:
             circles = np.uint16(np.around(circles))
             chosen = None
@@ -132,17 +169,17 @@ class CircleKiller():
             for i in circles[0, :]:
                 cv2.circle(frame, (i[0], i[1]), 1, (0, 100, 100), 3)
                 cv2.circle(frame, (i[0], i[1]), i[2], (255, 0, 255), 3)
-                dist_from_center = self.dist(i[0], i[1], 640, 360)
+                dist_from_center = self.dist(i[0], i[1], self.screen_width, self.screen_height)
                 if dist_from_center < closecircle_dist:
                     closecircle_dist = dist_from_center
                     chosen = i
-            cv2.line(frame, (640, 360), (chosen[0], chosen[1]), (0, 255, 0), 3)
+            cv2.line(frame, (self.screen_width, self.screen_height), (chosen[0], chosen[1]), (0, 255, 0), 3)
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(frame, str(closecircle_dist),
-                        (640, 360), font, 4, (0, 0, 0), 3)
-            dx = chosen[0] - 640
-            dy = 360-chosen[1]
-            dist_from_center = self.dist(chosen[0], chosen[1], 640, 360)
+                        (self.screen_width, self.screen_height), font, 4, (0, 0, 0), 3)
+            dx = chosen[0] - self.screen_width
+            dy = self.screen_height-chosen[1]
+            dist_from_center = self.dist(chosen[0], chosen[1], self.screen_width, self.screen_height)
             velocities = self.control_law(dx, dy, dist_from_center)
             print(velocities)
 
@@ -157,6 +194,7 @@ class CircleKiller():
             self.serialcomm.write(velocities.encode())
             self.serialcomm.close()
             return False
+        
         return True
         
         
--- src/main.py
+++ src/main.py
@@ -4,19 +4,27 @@ Created on Sat Aug 27 15:51:51 2022
 
 @author: Alyssa
 """
-import serial
+
+# Standard library imports.
 import time
 
-import numpy as np
+# Related third party imports.
 import cv2
+import numpy as np
+import serial
 
+# Local application/library specific imports.
 from circle_killer import CircleKiller
 
 
 
 if __name__ == "__main__":
     # initialize the CircleKiller object
-    ck = CircleKiller(arduino_port='COM7', baud_rate=115200, camera_id=0)
+    ck = CircleKiller(arduino_port='COM7', 
+                      baud_rate=115200, 
+                      camera_id=0,
+                      screen_center_width=640,
+                      screen_center_height=360)
     keep_running = True
     
     # continue to find new circles and shoot until the user types 'q'
